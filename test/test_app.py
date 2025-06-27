afb8c3113fc6c33f14b7dee2cc9e5225c721368b
Aditya Gupta <aditya.sg@metapercept.com>
2025-06-27 17:13:18 +0530

    Test  file updated

--- test/test_app.py
+++ test/test_app.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 from fastapi.testclient import TestClient
 
+
 client = TestClient(app)
 
 def test_summarize_success():
