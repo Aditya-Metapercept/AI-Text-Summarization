caf3aa1a99528bddab66dd0182e06d1c1769910c
Aditya Gupta <aditya.sg@metapercept.com>
2025-06-27 10:03:28 +0530

    summarization project done all the files and folders are created

--- src/app/summarizer.py
+++ src/app/summarizer.py
@@ -2,23 +2,35 @@ import os
 import logging
 import google.generativeai as genai
 from dotenv import load_dotenv
-
+import sys
+from src.logger import logging
+from src.exception import CustomException
 load_dotenv()
-logger = logging.getLogger("summarizer")
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")  # or "gemini-1.5-pro"
+try:
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    logging.info("Gemini Api key  configured successfully.")
+except Exception as e:
+    logging.error(f"Failed to configure Generative AI Key: {e}")
+    raise CustomException(e, sys)
 
+try:
+
+    model = genai.GenerativeModel("gemini-1.5-flash")  
+    logging.info("Gemini model loaded succesfully ")
+except Exception as e :
+    logging.error(f"Failed to loaded Gemini model {e}")
+    raise CustomException(e, sys)
 def summarize_text(text: str) -> str:
     try:
         if len(text.split()) < 10:
             return "Text too short to summarize."
 
-        prompt = f"Please summarize the following text and make sure the summary i seasy to understand :\n\n{text}"
+        prompt = f"Please summarize the following text and make sure the summary is easy to understand :\n\n{text}"
         response = model.generate_content(prompt)
 
         return response.text.strip()
     except Exception as e:
-        logger.error(f"Summarization failed: {e}")
+        logging.error(f"Summarization failed: {e}")
         return "Summary not available."
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import app
 from fastapi.testclient import TestClient
-from src.app import app
 
 client = TestClient(app)
 
